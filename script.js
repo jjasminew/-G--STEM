b1cffa64a3cf909a224f83eda287906ad4b75747
Jasmine Wongphatarakul <110138906+jjasminew@users.noreply.github.com>
2022-08-21 02:07:30 +0000

    Embedded data visualizations into the website and updated the data page.

--- script.js
+++ script.js
@@ -58,3 +58,7 @@ function mouseOutAbout() {
   //dots[slideIndex-1].className += " active";
   //setTimeout(showSlides, 2000); // Change image every 2 seconds
 //}
+
+//!function(){"use strict";window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();
+
+!function(){"use strict";window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();
