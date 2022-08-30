e6173f4eb833bc3dc72ba93c79bef01c949f92cb
Jasmine Wongphatarakul <110138906+jjasminew@users.noreply.github.com>
2022-08-30 03:06:05 +0000

    Made the pages responsive on the phone #2

--- script.js
+++ script.js
@@ -39,26 +39,4 @@ function mouseOutAbout() {
   document.getElementById("aboutTab").innerHTML = "About Us";
 }
 
-//let slideIndex = 0;
-//showSlides();
-
-//function showSlides() {
- // let i;
-  //let slides = document.getElementsByClassName("mySlides");
-  //let dots = document.getElementsByClassName("dot");
- // for (i = 0; i < slides.length; i++) {
-    //slides[i].style.display = "none";  
-  //}
-  //slideIndex++;
-  //if (slideIndex > slides.length) {slideIndex = 1}    
-  //for (i = 0; i < dots.length; i++) {
-    //dots[i].className = dots[i].className.replace(" active", "");
-  //}
-  //slides[slideIndex-1].style.display = "block";  
-  //dots[slideIndex-1].className += " active";
-  //setTimeout(showSlides, 2000); // Change image every 2 seconds
-//}
-
-//!function(){"use strict";window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();
-
-//!function(){"use strict";window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}(); (data visualizations code)
+!function(){"use strict";window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}()
